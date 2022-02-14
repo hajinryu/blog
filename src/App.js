8abdff60175c450909932b7b86e6a0a8be2c8cb5
유하진 <hjyoo@selab.co.kr>
2022-02-14 15:07:16 +0900

    입력값 update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Nav from './components/Nav';
 /* eslint-disable */
 
 function App() {
-  let posts = '블로그 글 제목'
   let [title, setTitle] = useState(['useState3', 'useState2', 'useState1']) //destructuring
   let [count, setCount] = useState(0);
   let [modal, setModal] = useState(false);
-  let [number,setNumber]=useState(0);
+  let [number, setNumber] = useState(0);
+  let [constext, setContext] = useState('');
 
   function changeTitle() {
     var newArray = [...title];
@@ -18,22 +18,39 @@ function App() {
     setTitle(newArray);
   }
 
+  const submit=(event)=>{
+    event.preventDefault()
+    setTitle(
+      [
+        ...title,constext
+      ]
+    )
+  }
 
   return (
     <div className="App">
       <Nav />
       {
-        title.map((글,i) => {
+        title.map((글, i) => {
           return (
             <div className='list' key={i}>
-              <h3 onClick={() => {setModal(!modal), setNumber(i)}}>{글}<span onClick={() => { setCount(count + 1) }}>👍</span>{count}</h3>
+              <h3 onClick={() => { setModal(!modal), setNumber(i) }}>{글}</h3>
+              <span onClick={() => { setCount(count + 1) }}>👍</span>{count}
               <p>2월 14일</p>
               <hr />
             </div>
           )
         })
       }
-      
+
+      <div className='publish'>
+        <form onSubmit={submit}>
+          <input onChange={(e) => setContext(e.target.value)} />
+          <button>저장</button>
+        </form>
+      </div>
+
+
       {
         modal === true
           ? <Modal title={title} number={number}></Modal>

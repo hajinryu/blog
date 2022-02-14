6a4be112b16128dfd42497ea611da4167bf045d4
유하진 <hjyoo@selab.co.kr>
2022-02-14 14:44:49 +0900

    Too many re-renders. React limits the number of renders to prevent an infinite loop.
    react onclick function에서 한개 이상의 함수를 사용할때는 () =>{} 괄호가 붙어야함

--- src/App.js
+++ src/App.js
@@ -8,42 +8,37 @@ function App() {
   let posts = '블로그 글 제목'
   let [title, setTitle] = useState(['useState3', 'useState2', 'useState1']) //destructuring
   let [count, setCount] = useState(0);
-  let[modal,setModal]=useState(false);
+  let [modal, setModal] = useState(false);
+  let [number,setNumber]=useState(0);
 
   function changeTitle() {
     var newArray = [...title];
-    newArray[0]='hi'
-    
+    newArray[0] = 'hi'
+
     setTitle(newArray);
   }
 
 
   return (
     <div className="App">
-      <Nav/>
-      <div className='list'>
-        <button onClick={changeTitle}>button</button>
-        <h3>{title[0]} <span onClick={() => { setCount(count + 1) }}>👍</span>{count}</h3>
-        <p>2월 14일</p>
-        <hr />
-      </div>
-      <div className='list'>
-        <h3>{title[1]}<span onClick={() => { setCount(count + 1) }}>👍</span>{count}</h3>
-        <p>2월 14일</p>
-        <hr />
-      </div>
-      <div className='list'>
-        <h3 onClick={()=> setModal(!modal)}>{title[2]}</h3>
-        <p>2월 14일</p>
-        <hr />
-      </div>
+      <Nav />
       {
-        modal === true
-        ? <Modal></Modal>
-        :null
+        title.map((글,i) => {
+          return (
+            <div className='list' key={i}>
+              <h3 onClick={() => {setModal(!modal), setNumber(i)}}>{글}<span onClick={() => { setCount(count + 1) }}>👍</span>{count}</h3>
+              <p>2월 14일</p>
+              <hr />
+            </div>
+          )
+        })
       }
       
-      
+      {
+        modal === true
+          ? <Modal title={title} number={number}></Modal>
+          : null
+      }
 
     </div>
   );
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,10 @@
 import '../App.css';
 import React, { useState } from 'react';
 
-const Modal=()=>{
+const Modal=(props)=>{
     return(
         <div className='modal'>
-        <h2>제목</h2>
+        <h2>{props.title[props.number]}</h2>
         <p>날짜</p>
         <p>내용</p>
       </div>
